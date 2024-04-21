3e4181430f35a2ac5877bc36b8b155ffcf7d4438
anton1dev <anton.kuchmasov.dev@gmail.com>
2024-04-21 09:01:26 +0300

    implemented users notifications

--- client.js
+++ client.js
@@ -1,6 +1,8 @@
 const net = require('net');
 const readLine = require('readline/promises');
 
+let id;
+
 const rl = readLine.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -22,6 +24,7 @@ const moveCursor = (dx, dy) => {
   });
 };
 
+
 const socket = net.createConnection(
   { host: '127.0.0.1', port: 3008 },
   async () => {
@@ -36,7 +39,7 @@ const socket = net.createConnection(
 
       await clearLine(0);
 
-      socket.write(message);
+      socket.write(`${id}-message-${message}`);
     };
 
     ask();
@@ -46,6 +49,15 @@ const socket = net.createConnection(
       console.log();
       await moveCursor(0, -1);
       await clearLine(0);
+
+      if (data.toString('utf-8').substring(0, 2) === 'id') {
+        id = data.toString('utf-8').substring(3);
+
+        console.log((`Your ID is ${id}! \n`));
+      } else {
+
+      }
+
       console.log(data.toString('utf-8'));
 
       ask();
--- server.js
+++ server.js
@@ -5,15 +5,39 @@ const server = net.createServer();
 const clients = [];
 
 server.on('connection', (socket) => {
+
   console.log(('new connection to server!'));
 
+  const clientId = clients.length + 1;
+  clients.map(client => {
+    client.socket.write(`User ${clientId} joined a chat!`);
+  })
+
+  socket.write(`id-${clientId}`);
+
   socket.on('data', (data) => {
-    clients.map(socket => {
-      socket.write(data);
+    const dataString = data.toString('utf-8');
+    const id = dataString.substring(0, dataString.indexOf('-'));
+    const message = dataString.substring(dataString.indexOf('-message-') + 9);
+
+    clients.map(client => {
+      client.socket.write(`> User ${id}: ${message}`);
     });
   });
 
-  clients.push(socket);
+  socket.on('end', () => {
+    clients.map(client => {
+      client.socket.write(`User ${clientId} left a chat!`)
+    })
+  });
+
+  socket.on('error', () => {
+    clients.map((client) => {
+      client.socket.write(`User ${clientId} left!`);
+    });
+  });
+
+  clients.push({ id: clientId.toString(), socket });
 });
 
 
