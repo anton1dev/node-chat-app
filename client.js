419b78c2723a4a87a8bdb8bae9a5a240213bc6f6
anton1dev <anton.kuchmasov.dev@gmail.com>
2024-04-21 08:06:55 +0300

    implemented all app functionality

--- client.js
+++ client.js
@@ -1,13 +1,59 @@
 const net = require('net');
+const readLine = require('readline/promises');
 
-const client = net.createConnection({ host: '127.0.0.1', port: 3008 }, () => {
-  console.log('connected to server!');
+const rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-client.on('close', () => {
-  console.log('closed!');
-})
+const clearLine = (dir) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.clearLine(dir, () => {
+      resolve();
+    });
+  });
+};
 
-client.end('end', () => {
+const moveCursor = (dx, dy) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.moveCursor(dx, dy, () => {
+      resolve();
+    });
+  });
+};
+
+const socket = net.createConnection(
+  { host: '127.0.0.1', port: 3008 },
+  async () => {
+    console.log('connected to server!');
+
+    const ask = async () => {
+
+
+      const message = await rl.question('Enter your message here > ');
+
+      await moveCursor(0, -1);
+
+      await clearLine(0);
+
+      socket.write(message);
+    };
+
+    ask();
+
+
+    socket.on('data', async (data) => {
+      console.log();
+      await moveCursor(0, -1);
+      await clearLine(0);
+      console.log(data.toString('utf-8'));
+
+      ask();
+    });
+
+  }
+);
+
+socket.on('end', () => {
   console.log('connection was ended!');
 })
--- server.js
+++ server.js
@@ -2,9 +2,20 @@ const net = require('net');
 
 const server = net.createServer();
 
+const clients = [];
+
 server.on('connection', (socket) => {
   console.log(('new connection to server!'));
-})
+
+  socket.on('data', (data) => {
+    clients.map(socket => {
+      socket.write(data);
+    });
+  });
+
+  clients.push(socket);
+});
+
 
 server.listen(3008, "127.0.0.1", () => {
   console.log('server is live', server.address());
